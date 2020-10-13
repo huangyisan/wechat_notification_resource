3d7ac980143c56d4cd064af1d143e07ebda8bd94
huangyisan <anonymousyisan@gmail.com>
2020-10-13 16:02:51 +0800

    update

--- assets/out.py
+++ assets/out.py
@@ -1,19 +1,3 @@
-#!/usr/bin/python
-# import json
-# import sys
-# import time
-#
-# def get_timestamp():
-#     return str(int(time.time()))
-#
-# def _check():
-#     timestamp = get_timestamp()
-#     return {"version": {"version": get_timestamp()}}
-#
-# if __name__ == "__main__":
-#     print(json.dumps(_check()))
-
-
 import json
 import sys
 import requests
